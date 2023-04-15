dee93b885c026cf43c5a76a26f33b796c1c2fcbc
SaIGyouKo <109269491+SaIGyouKo@users.noreply.github.com>
2023-04-15 18:38:56 +0800

    RENAME

--- main.go
+++ main.go
@@ -74,7 +74,7 @@ import (
 	//_ "github.com/FloatTech/ZeroBot-Plugin/plugin/bilibili"         // b站相关
 	//_ "github.com/FloatTech/ZeroBot-Plugin/plugin/book_review"      // 哀伤雪刃吧推书记录
 	//_ "github.com/FloatTech/ZeroBot-Plugin/plugin/cangtoushi"       // 藏头诗
-	//_ "github.com/FloatTech/ZeroBot-Plugin/plugin/choose"           // 选择困难症帮手
+	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/choose"           // 选择困难症帮手
 	//_ "github.com/FloatTech/ZeroBot-Plugin/plugin/chouxianghua"     // 说抽象话
 	//_ "github.com/FloatTech/ZeroBot-Plugin/plugin/chrev"            // 英文字符翻转
 	//_ "github.com/FloatTech/ZeroBot-Plugin/plugin/coser"            // 三次元小姐姐
@@ -143,7 +143,7 @@ import (
 	//_ "github.com/FloatTech/ZeroBot-Plugin/plugin/wangyiyun"        // 网易云音乐热评
 	//_ "github.com/FloatTech/ZeroBot-Plugin/plugin/warframeapi"      // warframeAPI插件
 	//_ "github.com/FloatTech/ZeroBot-Plugin/plugin/wenben"           // 文本指令大全
-	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/wenxinAI"         // 百度文心AI画图
+	//_ "github.com/FloatTech/ZeroBot-Plugin/plugin/wenxinAI"         // 百度文心AI画图
 	//_ "github.com/FloatTech/ZeroBot-Plugin/plugin/wife"             // 抽老婆
 	//_ "github.com/FloatTech/ZeroBot-Plugin/plugin/word_count"       // 聊天热词
 	//_ "github.com/FloatTech/ZeroBot-Plugin/plugin/wordle"           // 猜单词
